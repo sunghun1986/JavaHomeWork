e124eeb67a96e2347d7d1fae6616fda359050e72
sunghun1986 <sunghun1986z@naver.com>
2020-05-29 16:10:59 +0900

    시작

---
+++ Project0529/src/network/uni/ChatClient.java
@@ -0,0 +1,19 @@
+package network.uni;
+
+import javax.swing.JFrame;
+
+public class ChatClient extends JFrame{
+	
+	
+	
+	public ChatClient() {
+	
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		new ChatClient();
+	}
+	
+}
